12dc1eb4abb3266525ccdb945226bd9397daae83
Josephine Deng <jojod_27@yahoo.com>
2025-03-09 16:31:27 -0400

    add monolingual (en, spa) and multilingual 'not alphanum paragraph' taggers

--- python/dolma/taggers/punctuation.py
+++ python/dolma/taggers/punctuation.py
@@ -37,3 +37,74 @@ class NotAlphanumParagraphV1(BaseTagger):
             spans.append(Span(start=0, end=len(doc.text), type="all_punct", score=0))
 
         return DocResult(doc=doc, spans=spans)
+
+@TaggerRegistry.add("not_alphanum_paragraph_monolingual")
+class NotAlphanumParagraphMonolingual(BaseTagger):
+    def __init__(self, language: str = "en") -> None:
+        if language == "en":
+            self.re_has_alphanum = regex.compile(r"[a-zA-Z0-9]", regex.UNICODE)
+        elif language == "spa":
+            self.re_has_alphanum = regex.compile(r"\p{Latin}|\d", regex.UNICODE)
+        else:
+            print(f"Warning: Unsupported language '{language}'. Defaulting to multilingual mode.")
+            self.re_has_alphanum = regex.compile(r"\p{L}|\p{N}", regex.UNICODE)
+        
+        self.re_all_punctuation = regex.compile(
+            r"^("
+            r"\p{P}|"
+            r"\s|"
+            r"["
+            "\U0001f300-\U0001f64f"
+            "\U0001f680-\U0001f6ff"
+            "\u2600-\u26ff\u2700-\u27bf"
+            r"]+"
+            r")+$",
+            regex.UNICODE,
+        )
+
+    def predict(self, doc: Document) -> DocResult:
+        spans = []
+
+        for para in split_paragraphs(text=doc.text):
+            if self.re_has_alphanum.search(para.text):
+                continue
+
+            if self.re_all_punctuation.search(para.text):
+                spans.append(Span(start=para.start, end=para.end, type="all_punct", score=1))
+
+        if not spans:
+            spans.append(Span(start=0, end=len(doc.text), type="all_punct", score=0))
+
+        return DocResult(doc=doc, spans=spans)
+
+@TaggerRegistry.add("not_alphanum_paragraph_multilingual")
+class NotAlphanumParagraphMultilingual(BaseTagger):
+    def __init__(self) -> None:
+        self.re_has_alphanum = regex.compile(r"\p{L}|\p{N}", regex.UNICODE)
+        self.re_all_punctuation = regex.compile(
+            r"^("
+            r"\p{P}|"
+            r"\s|"
+            r"["
+            "\U0001f300-\U0001f64f"
+            "\U0001f680-\U0001f6ff"
+            "\u2600-\u26ff\u2700-\u27bf"
+            r"]+"
+            r")+$",
+            regex.UNICODE,
+        )
+
+    def predict(self, doc: Document) -> DocResult:
+        spans = []
+
+        for para in split_paragraphs(text=doc.text):
+            if self.re_has_alphanum.search(para.text):
+                continue
+
+            if self.re_all_punctuation.search(para.text):
+                spans.append(Span(start=para.start, end=para.end, type="all_punct", score=1))
+
+        if not spans:
+            spans.append(Span(start=0, end=len(doc.text), type="all_punct", score=0))
+
+        return DocResult(doc=doc, spans=spans)
